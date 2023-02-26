6463fc88f8cc470af96e810c8ed660e4ecf972a2
orenrimer <oren9rimer@gmail.com>
2023-02-26 19:07:16 +0200

    refactor code

--- src/converter.js
+++ src/converter.js
@@ -1,7 +1,3 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-const fs = require("fs");
-
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 ffmpeg.setFfmpegPath(ffmpegPath);
@@ -9,30 +5,17 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 const settings = require('../settings')
 
 
-function convertToMp4() {
-
-    // create a 10 seconds mp4 video showing the screenshot given in path
-    ffmpeg(settings.TMP_FILE)
-    .toFormat('mp4')
-    .videoCodec('libx264')
-    .size('640x?')
-    .loop(10)
-    .output(settings.OUT_FILE)
-    .on('error',(err) => { throw err })
-    .on('end',async () => {
-        // i have also uploaded the final result to a s3 bucket 
-        const fileContent = fs.readFileSync(settings.OUT_FILE);
-        var params = {
-            Bucket: process.env.BUCKET_NAME,
-            Key: `${new Date().toISOString()}_screenshot.mp4`,
-            Body: fileContent
-        };
-
-        await s3.upload(params).promise();
+function createMP4(){
+    return new Promise((resolve,reject) => {
+        ffmpeg(settings.TMP_FILE)
+        .toFormat('mp4')
+        .videoCodec('libx264')
+        .size('640x?')
+        .loop(10)
+        .save(settings.OUT_FILE)
+        .on('error',(err) => { reject(new Error(err)) })
+        .on('end', () => { resolve() })
     })
-    .run();
-    // return a path to the final product
 }
 
-
-module.exports = convertToMp4;
+module.exports = createMP4;
--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const playwright = require('playwright-aws-lambda');
-const convertToMp4 = require('./converter');
-const settings = require('../settings')
+const AWS = require("aws-sdk");
 const fs = require("fs");
+
+const createMP4 = require('./converter');
+const settings = require('../settings')
+const s3 = new AWS.S3();
 
 
 exports.handler =  async(event, context, callback) => {  
   const data = JSON.parse(event.body);
+
   // check if valid payload
   if (!data || !data.hasOwnProperty('url')) return {
       statusCode: 400,
@@ -24,8 +28,21 @@ exports.handler =  async(event, context, callback) => {
     
     // store the screenshot in a temp folder
     await page.screenshot({path : settings.TMP_FILE});
-    convertToMp4();
 
+    // wait for output file to be created
+    await createMP4();
+
+    // i have also uploaded the final result to a s3 bucket 
+    const fileContent = fs.readFileSync(settings.OUT_FILE);
+    var params = {
+        Bucket: process.env.BUCKET_NAME,
+        Key: `${new Date().toISOString()}_screenshot.mp4`,
+        Body: fileContent
+    };
+
+    await s3.upload(params).promise();
+
+    // return the path to the output file in the response
     return {
       statusCode: 200,
       body: JSON.stringify({file: settings.OUT_FILE})
